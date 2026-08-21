2c8c6534794fdc0aa39aa2a2b07ac376e675fedc
Aravindhan <104446384+Aravindh-dev12@users.noreply.github.com>
2026-08-21 20:16:42 +0530

    feat: mount Scene OS interface layer

--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import SceneOS from './components/SceneOS.vue'
 import './styles.css'
 import './animation.css'
+import './scene-os.css'
 
 createApp(App).mount('#app')
+
+const sceneRoot = document.createElement('div')
+sceneRoot.id = 'scene-os-root'
+document.body.appendChild(sceneRoot)
+createApp(SceneOS).mount(sceneRoot)
