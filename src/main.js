dc9606d66131a2f1b3e97e2514d6891de8b949d6
Aravindhan <104446384+Aravindh-dev12@users.noreply.github.com>
2026-08-23 08:35:36 +0530

    Remove obsolete middle alignment override

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './styles.css'
 import './experience.css'
-import './alignment.css'
 import './finale.css'
 
 createApp(App).mount('#app')
