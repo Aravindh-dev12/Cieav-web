bb80cba2490e9e951dbc5edbfac536ee1b70d893
Aravindhan <104446384+Aravindh-dev12@users.noreply.github.com>
2026-08-23 08:27:48 +0530

    Load middle-section alignment fixes

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './styles.css'
 import './experience.css'
+import './alignment.css'
 import './finale.css'
 
 createApp(App).mount('#app')
