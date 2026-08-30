7df449a1d122507faad864e27e3c84688259971f
Aravindhan <104446384+Aravindh-dev12@users.noreply.github.com>
2026-08-30 19:09:32 +0530

    Load only stairs journey styles

--- src/main.js
+++ src/main.js
@@ -1,8 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './styles.css'
-import './experience.css'
-import './finale.css'
 import './atlas-inspiration.css'
 
 createApp(App).mount('#app')
